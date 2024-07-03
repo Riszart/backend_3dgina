bae5eec2b4fb56370ff377e8e0fb0404fee495e7
riszart <22.r.d.v.c.12@gmail.com>
2024-07-02 20:54:48 -0500

    secund commit secund commit Please enter the commit message for your changes. Lines starting secund commit with '#' will be ignored, and an empty message aborts the commit. secund commit secund commit On branch main secund commit Changes to be committed: secund commit	renamed:    json/jabon.json -> json/jabones.json secund commit	new file:   package-lock.json secund commit	deleted:    script.js secund commit	new file:   script.mjs secund commit

---
+++ script.mjs
@@ -0,0 +1,32 @@
+const data_global= []
+
+const array_data_backend_all = {
+  aguas:'https://raw.githubusercontent.com/Riszart/backend_3dgina/main/json/aguas.json',
+  bebidas_gasificadas:'https://raw.githubusercontent.com/Riszart/backend_3dgina/main/json/bebidas_gasificadas.json',
+  detergentes:'https://raw.githubusercontent.com/Riszart/backend_3dgina/main/json/detergentes.json',
+  // jabones:'https://raw.githubusercontent.com/Riszart/backend_3dgina/main/json/jabones.json',
+  lejias:'https://raw.githubusercontent.com/Riszart/backend_3dgina/main/json/lejias.json',
+  frugos:'https://raw.githubusercontent.com/Riszart/backend_3dgina/main/json/frugos.json',
+  // energisantes:'',
+  // papeles_de_limpiesa:'',
+}
+
+
+Object.values(array_data_backend_all).forEach(url => {
+  loadData(url)
+});
+async function loadData(URL){
+  const response = await fetch(URL)
+  data = await response.json();
+  data.forEach(element => {
+    data_global.push(element)
+  });
+  console.log(data_global)
+}
+
+
+function allProducts(data){
+  
+} 
+
+module.exports = array_data_backend_all
